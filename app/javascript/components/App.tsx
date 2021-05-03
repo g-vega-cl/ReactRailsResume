1114327a70c9c8d07b8dd3f638d0ea07a34c9cf7
g-vega-cl <g.vega.cl@gmail.com>
2021-05-03 17:54:21 -0500

    Added navbar component

--- app/javascript/components/App.tsx
+++ app/javascript/components/App.tsx
@@ -2,10 +2,11 @@ import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "../components/Home/Home";
 import Project from "../components/Project/Project";
-
+import Navbar from '../components/Navbar/Navbar';
 const App = () => {
   return (
     <BrowserRouter>
+      <Navbar/>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/projects/:slug" component={Project} />
